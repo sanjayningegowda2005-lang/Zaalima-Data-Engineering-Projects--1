4c45f0d8d646ae0396f710fc5787393d78913d6e
HARSHA VARIKUTI <harsha123vr@gmail.com>
2026-08-15 14:43:54 +0530

    addtest_connection fun

--- DATAB.py
+++ DATAB.py
@@ -6,11 +6,20 @@ def get_postgre_engine(user="postgres",password="Harsha@1131",host="localhost",p
     con_str="postgresql://%s:%s@%s/%s" % (user,password,host,port,db_name)
     engine = create_engine(con_str)
     return engine
-if __name__=="main":
+def test_connection(engine):
+    try:
+        with engine.connect() as conn:
+            print("Connection successful")
+    except Exception as e:
+        print("Connection failed:", e)
+if __name__ == "__main__":
     sql_engine=get_sqlite_engine("mydb.sqlite")
     print("sql engine created:", sql_engine)
+    test_connection(sql_engine)
     postgre_engine=get_postgre_engine()
     print("postgreSQL engine created:",postgre_engine)
+    test_connection(postgre_engine)
+
 
 
 
